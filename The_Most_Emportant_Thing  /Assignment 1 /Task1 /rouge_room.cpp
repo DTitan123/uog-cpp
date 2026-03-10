fde593a60256c158b1ade94368146bfc75aa370f
Harry <hdjpe@icloud.com>
2026-03-10 15:22:03 +0000

    Added team project folder and started rogue room

---
+++ The_Most_Emportant_Thing  /Assignment 1 /Task1 /rouge_room.cpp
@@ -0,0 +1,27 @@
+//
+//  rouge_room.cpp
+//  Advanced Software Assignment
+//
+//  Created by Harry Powell-Evans on 27/01/2026.
+//
+
+#include <iostream>
+
+using namespace std;
+
+void draw_room(int w, int h, int x, int y){
+    
+}
+
+int main(){
+    int room[] = {2, 2, 0, 1};
+    for (int i = 0; i < size(room); i++){
+            
+    }
+    
+        
+}
+        
+
+ 
+
