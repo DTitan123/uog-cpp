95ea03a1aaf682c383707f256a16797c30eb11e6
Harry <hdjpe@icloud.com>
2026-03-15 15:15:56 +0000

    fine tune

--- The_Most_Emportant_Thing  /Assignment 1 /Task1 /rouge_room.cpp
+++ The_Most_Emportant_Thing  /Assignment 1 /Task1 /rouge_room.cpp
@@ -12,7 +12,7 @@
 
 using namespace std;
 
-class room{
+class Room{
 private:
     int width;
     int height;
@@ -20,18 +20,20 @@ private:
     int y;
     char floor_marker;
     char player_marker;
-    char wall_marker;
+    char wall_s_marker;
+    char wall_t_marker;
     char corner_marker;
 
 public:
-    room(int w, int h, char floor = '.', char player = '@', char wall = '|', char corner = '+'){
+    Room(int w, int h, char floor = '.', char player = '@', char wall_s = '|', char wall_t = '-', char corner = '+'){
         width = w;
         height = h;
         x=0;
         y=0;
         floor_marker = floor;
         player_marker = player;
-        wall_marker = wall;
+        wall_s_marker = wall_s;
+        wall_t_marker = wall_t;
         corner_marker = corner;
     }
     
@@ -39,8 +41,9 @@ public:
     void set_player_marker(char marker){
         player_marker = marker;
     }
-    void set_wall_marker(char marker){
-        wall_marker = marker;
+    void set_wall_marker(char marker_s, char marker_t){
+        wall_s_marker = marker_s;
+        wall_t_marker = marker_t;
     }
     void set_floor_marker(char marker){
         floor_marker = marker;
@@ -52,21 +55,21 @@ public:
     //Draw Method 
     void draw(){
         //Draw the top wall
-        cout << corner_marker << string(width, wall_marker) << corner_marker <<end l;
+        cout << corner_marker << string(width, wall_t_marker) << corner_marker << endl;
 
         for (int i=0; i < height; i++){
-            if (int i !=y){
+            if (i != y){
                 //Draw a row of floor
-                cout << wall_marker << string(width, floor_marker) << wall_marker << endl;
+                cout << wall_s_marker << string(width, floor_marker) << wall_s_marker << endl;
             }
             else{
                 //Draw a row with the player
-                cout << wall_marker << string(x, floor_marker) << player_marker << string(width-x-1, floor_marker) << wall_marker << endl;
+                cout << wall_s_marker << string(x, floor_marker) << player_marker << string(width-x-1, floor_marker) << wall_s_marker << endl;
             }
         }
 
         //Draw the bottom wall
-        cout << corner_marker << string(width, wall_marker) << corner_marker << endl;
+        cout << corner_marker << string(width, wall_t_marker) << corner_marker << endl;
     }
 
     //Movement
@@ -108,9 +111,46 @@ public:
 };
 
 int main(){
-    int room[] = {2, 2, 0, 1};
-    for (int i = 0; i < size(room); i++){
-            
-    }
     
+    //Creat a room with dimensions 8X6
+
+    Room room(8, 6);
+
+    while (true){
+        //draw the room 
+        room.draw();
+
+        //Get user input
+        cout << "Enter command (w/a/s/d & x to exit): ";
+        char input;
+        cin >> input;   
+
+        //Handle input
+        if (input == 'w'){
+            room.up();
+        }
+        else if (input == 'a'){
+            room.left();
+        }
+        else if (input == 's'){
+            room.down();
+        }
+        else if (input == 'd'){
+            room.right();
+        }
+        else if (input == 'x'){
+            cout << "Exiting game..." << endl;
+            break;
+        }
+        else{
+            cout << "Invalid command!" << endl;
+        }
+
+        //sleep for 0.5 seconds 
+        this_thread::sleep_for(chrono::milliseconds(500));
+
+        // Clear the screen (ANSI escape code
+        cout << "\033[2J\033[1;1H";
+    }
+    return 0;
 }
